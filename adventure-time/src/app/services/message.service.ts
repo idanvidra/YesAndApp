9898797a0537e1ad3e9ece197562a42137aa53c3
idanvidra <idan.vidra@gmail.com>
2021-12-07 18:38:27 +0200

    preparing the project for deployment

--- adventure-time/src/app/services/message.service.ts
+++ adventure-time/src/app/services/message.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-const BASEURL = "http://localhost:3000/api/adventuretime";
+const BASEURL = environment.apiHost;
 
 @Injectable({
   providedIn: 'root'
